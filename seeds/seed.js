19b15f7d478aba4f0071eba5f00702155ff88390
konnenhannaford <konnen23@gmail.com>
2022-02-12 23:48:13 +1100

    updated seed files

---
+++ seeds/seed.js
@@ -0,0 +1,31 @@
+const sequelize = require('../config/connection');
+const { User, Post, Comment } = require('../models');
+
+const userSeed = require('./users.json');
+const postSeed = require('./posts.json');
+const commentSeed = require('./commenting.json')
+
+const seedDatabase = async () => {
+  await sequelize.sync({ force: true });
+
+  const users = await User.bulkCreate(userSeed, {
+    individualHooks: true,
+    returning: true,
+  });
+
+  for (const post of postSeed) {
+    await Post.create({
+      ...post
+    });
+  }
+
+  for (const comment of commentSeed){
+    await Comment.create({
+      ...comment
+    });
+  }
+
+  process.exit(0);
+};
+
+seedDatabase();
